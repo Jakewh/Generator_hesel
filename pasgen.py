ad3f3b8a16f6fef680118a8756cfe51fb23755ac
JakeWh <kolarkuba@gmail.com>
2021-11-25 09:46:42 +0100

    dalsi uprava

--- pasgen.py
+++ pasgen.py
@@ -8,17 +8,18 @@ print(color.RED + "===================\n॥ Generátor hesel ॥\n==============
 pismena = string.ascii_lowercase + string.ascii_uppercase   # importujeme malá i velká oísmena z ascii
 cisla = string.digits   # importujeme čísla z ascii
 znaky = string.punctuation  # importujeme znaky z ascii
+delka = int(input("Zadejte požadovanou délku hesla: "))
 
 print("Zadej typ požadovaného složení hesla:")
 typ_hesla = int(input("1) Písmena, čísla i znaky\t2) Písmena a čísla\t3) Písmena\n"))   #volba typu hesla (1,2,3)
 if typ_hesla == 1:
-    heslo = random.SystemRandom().choice(pismena + cisla + znaky)
+    heslo = "".join(random.SystemRandom().choice(pismena + cisla + znaky, * delka)) # pojebaná délka hesla pořád nejde
     print("Vygenerované heslo je:", heslo)
 elif typ_hesla == 2:
-        heslo = random.SystemRandom().choice(pismena + cisla)
+        heslo = "".join(random.SystemRandom().choice(pismena + cisla))
         print("Vygenerované heslo je:", heslo)
 else:
-    heslo = random.SystemRandom().choice(pismena)
+    heslo = "".join(random.SystemRandom().choice(pismena))
     print("Vygenerované heslo je:", heslo)
 print("Děkujeme za použití generátoru hesel. Stisknutím klávesy jej zavřete.")
 input()
