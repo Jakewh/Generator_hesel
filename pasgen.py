888857a59edca6a05c05b82ef952516a40b89db3
JakeWh <kolarkuba@gmail.com>
2021-11-25 08:50:17 +0100

    pridany barvy

--- pasgen.py
+++ pasgen.py
@@ -1,6 +1,10 @@
 import random, string
 
-print("====PASGEN====\ngenerátor hesel\n===============")
+class color:
+    RED = "\033[91m"
+    END = "\033[0m"
+
+print(color.RED + "===================\n॥ Generátor hesel ॥\n===================" + color.END)
 pismena = string.ascii_lowercase + string.ascii_uppercase   # importujeme malá i velká oísmena z ascii
 cisla = string.digits   # importujeme čísla z ascii
 znaky = string.punctuation  # importujeme znaky z ascii
@@ -8,9 +12,8 @@ znaky = string.punctuation  # importujeme znaky z ascii
 print("Zadej typ požadovaného složení hesla:")
 typ_hesla = int(input("1) Písmena, čísla i znaky\t2) Písmena a čísla\t3) Písmena\n"))   #volba typu hesla (1,2,3)
 if typ_hesla == 1:
-    for i in range(8):
-        heslo = random.SystemRandom().choice(pismena + cisla + znaky)
-        print("Vygenerované heslo je:", heslo)
+    heslo = random.SystemRandom().choice(pismena + cisla + znaky)
+    print("Vygenerované heslo je:", heslo)
 elif typ_hesla == 2:
         heslo = random.SystemRandom().choice(pismena + cisla)
         print("Vygenerované heslo je:", heslo)
